0004d7ef9a18b85f0cae80c287d285d08fc9d455
Satyasai Esarapu <esarapusathyasai603@gmail.com>
2024-06-28 17:13:09 +0530

    Update app.py

--- flask_app/app.py
+++ flask_app/app.py
@@ -6,7 +6,7 @@ import numpy as np
 app = Flask(__name__)
 
 # Load model
-with open('../flask_app/model.pkl', 'rb') as f:
+with open('model.pkl', 'rb') as f:
     model = pickle.load(f)
 
 @app.route('/')
